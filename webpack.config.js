a4c20a8b0a378bb5ffbc76513673d8e417430f9d
Rikstone <tarikbouari@gmail.com>
2022-08-14 17:28:42 +0000

    update homepage

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
     }),
   ],
 
@@ -20,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
