59e8c857352973a8afc11d4e44de69f062be13c1
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-16 13:23:04 -0700

    Define Event

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import './style.css';
 const colorChange = document.querySelector('.head');
 
 colorChange.style.color = 'blue';
+
+const getStarted = document.getElementById('btn-start');
+getStarted.addEventListener('click', () => {
+  const start = document.querySelector('.container');
+  start.classList.add('none');
+  const dashboard = document.querySelector('.dashboard');
+  dashboard.classList.remove('none');
+});
