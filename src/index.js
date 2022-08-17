c6220b77796e11b82680cbb183abf3f0e43700e9
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-17 10:53:56 -0700

    Give getStarted Button A Type

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const colorChange = document.querySelector('.head');
 colorChange.style.color = 'blue';
 
 const getStarted = document.getElementById('btn-start');
-getStarted.addEventListener('click', () => {
+getStarted.addEventListener('click', (e) => {
+  e.preventDefault();
   const start = document.querySelector('.container');
   start.classList.add('none');
   const dashboard = document.querySelector('.dashboard');
