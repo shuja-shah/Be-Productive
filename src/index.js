46248d35c8f4e150e703260ce0bbb6c680bc273b
Rikstone <tarikbouari@gmail.com>
2022-08-21 04:09:13 -0700

    add display block to aside

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import './style.css';
+import "./style.css";
 
-const colorChange = document.querySelector('.head');
+const getStarted = document.getElementById("btn-start");
 
-colorChange.style.color = 'blue';
-
-const getStarted = document.getElementById('btn-start');
-getStarted.addEventListener('click', (e) => {
+getStarted.addEventListener("click", (e) => {
   e.preventDefault();
-  const start = document.querySelector('.container');
-  start.classList.add('none');
-  const dashboard = document.querySelector('.dashboard');
-  dashboard.classList.remove('none');
+  const start = document.querySelector(".container");
+  start.classList.add("none");
+  const dashboard = document.querySelector(".dashboard");
+  dashboard.classList.remove("none");
+  const particles = document.querySelector(".particles");
+  particles.classList.add("none");
+  const sideBar = document.querySelector("aside");
+  sideBar.classList.add("block");
 });
